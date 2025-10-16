b48452664e8cfd114a21c063389083ba822c94a7
silviaperelli <silviaperelli02@gmail.com>
2025-10-16 11:59:26 +0200

    Tree to Representation added

--- src/main/java/jgea/Problem.java
+++ src/main/java/jgea/Problem.java
@@ -6,14 +6,13 @@ import io.github.ericmedvet.jgea.core.representation.grammar.string.StringGramma
 import io.github.ericmedvet.jgea.core.representation.tree.Tree;
 import jgea.representation.PipelineRepresentation;
 import jgea.representation.RepresentationToLiebreQuery;
+import jgea.representation.TreeToRepresentation;
 import query.Query;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.*;
 import java.util.function.Function;
-
-import static jgea.utils.TreeUtils.*;
 
 public class Problem implements GrammarBasedProblem<String, Query>,
         TotalOrderQualityBasedProblem<Query, Double> {
@@ -41,88 +40,21 @@ public class Problem implements GrammarBasedProblem<String, Query>,
                 // First mapping from tree to PipelineRepresentation
                 List<PipelineRepresentation.OperatorNode> operators = new ArrayList<>();
                 // Start recursive parsing from the root of the tree
-                parsePipelineNode(tree, operators);
+                TreeToRepresentation firstMapper = new TreeToRepresentation();
+                firstMapper.parsePipelineNode(tree, operators);
                 PipelineRepresentation intermediateRepr = new PipelineRepresentation(operators);
 
                 System.out.println("[Pipeline Generated] " + intermediateRepr);
 
                 // Second mapping from PipelineRepresentation to Query Liebre
-                RepresentationToLiebreQuery mapper = new RepresentationToLiebreQuery();
+                RepresentationToLiebreQuery secondMapper = new RepresentationToLiebreQuery();
                 String tempOutputFile = "src/main/resources/datasets/testBestQuery.csv";
-                return mapper.translate(intermediateRepr, this.inputCsvPath, tempOutputFile);
+                return secondMapper.translate(intermediateRepr, this.inputCsvPath, tempOutputFile);
             }catch(Exception e){
                 System.err.println(("Error during mapping process"));
                 return null;
             }
         };
-    }
-
-    // Parse a node <pipeline> recursively, add found operators to a list
-    private void parsePipelineNode(Tree<String> pipelineNode, List<PipelineRepresentation.OperatorNode> operators) {
-
-        // Look for the child node <filter> and <pipeline>
-        Tree<String> filterNode = null;
-        Tree<String> nextPipelineNode = null;
-
-        for (Tree<String> child : pipelineNode) {
-            if ("<filter>".equals(child.content())) {
-                filterNode = child;
-            } else if ("<pipeline>".equals(child.content())) {
-                nextPipelineNode = child;
-            }
-        }
-
-        // The tree is invalid if there isn't a node <filter>
-        if (filterNode == null) {
-            return;
-        }
-
-        // Parse filter node
-        PipelineRepresentation.OperatorNode operator = parseFilterNode(filterNode);
-        if (operator != null) {
-            operators.add(operator);
-        }
-
-        // Check if the pipeline is finished otherwise recursively calls the method and continue parsing the pipeline
-        if (nextPipelineNode != null) {
-            parsePipelineNode(nextPipelineNode, operators);
-        }
-    }
-
-
-    // Parse a single filter node
-    private PipelineRepresentation.OperatorNode parseFilterNode(Tree<String> filterNode) {
-        String attribute = null;
-        String operatorString = null;
-        Tree<String> valueNode = null;
-
-        // Look for the children in the node
-        for (Tree<String> child : filterNode) {
-            switch (child.content()) {
-                case "<attribute>" -> attribute = findFirstTerminal(child);
-                case "<operator>" -> operatorString = findFirstTerminal(child);
-                case "<value>" -> valueNode = child;
-            }
-        }
-
-        if (attribute == null || operatorString == null || valueNode == null)
-            return null;
-
-        // Mapping from tree to Pipeline Representation
-        try {
-            // Collect all the leaves (digit and .) under the node <value> and join them to reconstruct the number
-            List<String> leaves = valueNode.visitLeaves();
-            String valueString = String.join("", leaves);
-            double value = Double.parseDouble(valueString);
-            attribute = attribute.replace("'", "");
-            PipelineRepresentation.Operator op = PipelineRepresentation.fromString(operatorString);
-            PipelineRepresentation.Condition condition = new PipelineRepresentation.Condition(attribute, op, value);
-
-            return new PipelineRepresentation.OperatorNode("FILTER", condition);
-        } catch (Exception e) {
-            System.err.printf("[Parser] Error parsing filter node: %s\n", e.getMessage());
-            return null;
-        }
     }
 
     @Override
---
+++ src/main/java/jgea/representation/TreeToRepresentation.java
@@ -0,0 +1,77 @@
+package jgea.representation;
+
+import io.github.ericmedvet.jgea.core.representation.tree.Tree;
+
+import java.util.List;
+
+import static jgea.utils.TreeUtils.findFirstTerminal;
+
+public class TreeToRepresentation {
+
+    // Parse a node <pipeline> recursively, add found operators to a list
+    public void parsePipelineNode(Tree<String> pipelineNode, List<PipelineRepresentation.OperatorNode> operators) {
+
+        // Look for the child node <filter> and <pipeline>
+        Tree<String> filterNode = null;
+        Tree<String> nextPipelineNode = null;
+
+        for (Tree<String> child : pipelineNode) {
+            if ("<filter>".equals(child.content())) {
+                filterNode = child;
+            } else if ("<pipeline>".equals(child.content())) {
+                nextPipelineNode = child;
+            }
+        }
+
+        // The tree is invalid if there isn't a node <filter>
+        if (filterNode == null) {
+            return;
+        }
+
+        // Parse filter node
+        PipelineRepresentation.OperatorNode operator = parseFilterNode(filterNode);
+        if (operator != null) {
+            operators.add(operator);
+        }
+
+        // Check if the pipeline is finished otherwise recursively calls the method and continue parsing the pipeline
+        if (nextPipelineNode != null) {
+            parsePipelineNode(nextPipelineNode, operators);
+        }
+    }
+
+    // Parse a single filter node
+    private PipelineRepresentation.OperatorNode parseFilterNode(Tree<String> filterNode) {
+        String attribute = null;
+        String operatorString = null;
+        Tree<String> valueNode = null;
+
+        // Look for the children in the node
+        for (Tree<String> child : filterNode) {
+            switch (child.content()) {
+                case "<attribute>" -> attribute = findFirstTerminal(child);
+                case "<operator>" -> operatorString = findFirstTerminal(child);
+                case "<value>" -> valueNode = child;
+            }
+        }
+
+        if (attribute == null || operatorString == null || valueNode == null)
+            return null;
+
+        // Mapping from tree to Pipeline Representation
+        try {
+            // Collect all the leaves (digit and .) under the node <value> and join them to reconstruct the number
+            List<String> leaves = valueNode.visitLeaves();
+            String valueString = String.join("", leaves);
+            double value = Double.parseDouble(valueString);
+            attribute = attribute.replace("'", "");
+            PipelineRepresentation.Operator op = PipelineRepresentation.fromString(operatorString);
+            PipelineRepresentation.Condition condition = new PipelineRepresentation.Condition(attribute, op, value);
+
+            return new PipelineRepresentation.OperatorNode("FILTER", condition);
+        } catch (Exception e) {
+            System.err.printf("[Parser] Error parsing filter node: %s\n", e.getMessage());
+            return null;
+        }
+    }
+}
